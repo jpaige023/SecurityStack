b39d53f9f4c850e46c4427b5e2e29e293d3955d8
stmosher <smosher2005@yahoo.com>
2017-11-02 15:36:39 +0000

    static configs - too long to boot new; will power on instead

---
+++ TEMPLATES/Cloud/AWS/vpc_security_stack/az_a_2_var.tf
@@ -0,0 +1,37 @@
+/*
+  IP addresses
+*/
+variable "az_a_asav_e0_2" {
+    # range 51 to 59
+    default = "172.31.1.52"
+}
+
+variable "az_a_asav_e1_2" {
+    # range 11 to 19
+    default = "172.31.3.12"
+}
+
+variable "az_a_asav_e2_2" {
+    # range 51 to 59
+    default = "172.31.4.52"
+}
+
+variable "az_a_ftd_e0_2" {
+    # range 101 to 109
+    default = "172.31.1.102"
+}
+
+variable "az_a_ftd_e1_2" {
+    # range 151 to 159
+    default = "172.31.1.152"
+}
+
+variable "az_a_ftd_e2_2" {
+    # range 101 to 109
+    default = "172.31.4.102"
+}
+
+variable "az_a_ftd_e3_2" {
+    # range 11 to 19
+    default = "172.31.5.12"
+}
