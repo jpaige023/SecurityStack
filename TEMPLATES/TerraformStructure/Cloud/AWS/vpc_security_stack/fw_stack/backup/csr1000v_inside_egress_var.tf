96a339fb6695ad630e8bcbe978200717fd1261a7
stmosher <smosher2005@yahoo.com>
2017-11-09 18:32:37 +0000

    cleaned code

--- TEMPLATES/TerraformStructure/Cloud/AWS/vpc_security_stack/fw_stack/backup/csr1000v_inside_egress_var.tf
+++
@@ -1,5 +0,0 @@
-variable "csr1000v_inside_egress_${var.device_number}_e0" {}
-
-variable "csr1000v_inside_egress_${var.device_number}_e1" {}
-
-variable "csr1000v_inside_egress_${var.device_number}_e2" {}
--- TEMPLATES/TerraformStructure/Cloud/AWS/vpc_security_stack/fw_stack/csr1000v_outside_ingress_0.tf
+++ TEMPLATES/TerraformStructure/Cloud/AWS/vpc_security_stack/fw_stack/csr1000v_outside_ingress_0.tf
@@ -1,8 +1,8 @@
-variable "csr1000v_outside_egress_0_e0" {}
-variable "csr1000v_outside_egress_0_e1" {}
-variable "csr1000v_outside_egress_0_e2" {}
+variable "csr1000v_outside_ingress_0_e0" {}
+variable "csr1000v_outside_ingress_0_e1" {}
+variable "csr1000v_outside_ingress_0_e2" {}
 
-module "csr1000v_outside_egress" {
+module "csr1000v_outside_ingress" {
   source                = "./modules/csr1000v_outside"
   region                = "${var.region}"
   subnet_public         = "${module.base.subnet_public}"
@@ -12,7 +12,7 @@ module "csr1000v_outside_egress" {
   aws_key_name          = "${var.aws_key_name}"
   SG_SSH_IPSEC          = "${module.base.SG_SSH_IPSEC}"
   SG_All_Traffic        = "${module.base.SG_All_Traffic}"
-  csr1000v_outside_e0   = "${var.csr1000v_outside_egress_0_e0}"
-  csr1000v_outside_e1   = "${var.csr1000v_outside_egress_0_e1}"
-  csr1000v_outside_e2   = "${var.csr1000v_outside_egress_0_e2}"
+  csr1000v_outside_e0   = "${var.csr1000v_outside_ingress_0_e0}"
+  csr1000v_outside_e1   = "${var.csr1000v_outside_ingress_0_e1}"
+  csr1000v_outside_e2   = "${var.csr1000v_outside_ingress_0_e2}"
 }
