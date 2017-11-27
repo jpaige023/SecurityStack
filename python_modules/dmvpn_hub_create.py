7b6bc7ee52a7cbf11a80196dbc67033f684eb864
stmosher <smosher2005@yahoo.com>
2017-11-27 23:32:08 +0000

    created DM/dmvpn hub addresses yaml file

--- python_modules/dmvpn_hub_create.py
+++ python_modules/dmvpn_hub_create.py
@@ -1,26 +1,31 @@
-import simplejson as json
+import yaml
+import io
 
 def main(tfstate_dictionary):
 # def main():
-#     tfstate_dictionary = {"dmvpn_tunnel": "1", "ip_a": "1.1.1.31", "router_a_address_g1": "10.1.1.31"}
+#     tfstate_dictionary = {"dmvpn_tunnel": "1", "ip_a": "1.1.1.31", "router_a_address_g1": "10.1.1.31", "tunnel_address": "10.254.10.10"}
 
-    # Add hub ip address to hub list
-    with open('DB/dmvpn_tunnel_nhs_addresses.json') as json_data:
-        dictionary_initial_read = json.load(json_data)
+    # Add hub ip addresses to dmvpn_tunnel_nhs_addresses
+    with open("DB/dmvpn_tunnel_nhs_addresses.yml", 'r') as yml_data:
+        dictionary_initial_read = yaml.load(yml_data)
+
     dictionary_nhs_full = {}
-    if tfstate_dictionary["dmvpn_tunnel"] in dictionary_initial_read:
-        dictionary_nhs_addresses_initial = dictionary_initial_read[tfstate_dictionary["dmvpn_tunnel"]]
-        dictionary_nhs_addresses_updated = dictionary_initial_read[tfstate_dictionary["dmvpn_tunnel"]]
-        dictionary_nhs_addresses_updated[tfstate_dictionary["router_a_address_g1"]] = tfstate_dictionary["ip_a"]
+    # If hub tunnel x is already in there, add the addresses for the new hub site
+    if tfstate_dictionary["dmvpn_tunnel"] in dictionary_initial_read["dmvpn_addresses"]:
+        nhs_updates = {'dmvpn_address': tfstate_dictionary["tunnel_address"], 'public': tfstate_dictionary["ip_a"],
+                        'private': tfstate_dictionary["router_a_address_g1"]}
         dictionary_nhs_full.update(dictionary_initial_read)
-        dictionary_nhs_full[tfstate_dictionary["dmvpn_tunnel"]].update(dictionary_nhs_addresses_updated)
+        dictionary_nhs_full["dmvpn_addresses"][tfstate_dictionary["dmvpn_tunnel"]].append(nhs_updates)
+    # If hub tunnel x is not there, add tunnel x plus the addresses
     else:
-        dictionary_nhs_full[tfstate_dictionary["dmvpn_tunnel"]] = {
-            tfstate_dictionary["router_a_address_g1"]: tfstate_dictionary["ip_a"]}
+        nhs_updates = [{'dmvpn_address': tfstate_dictionary["tunnel_address"], 'public': tfstate_dictionary["ip_a"],
+                        'private': tfstate_dictionary["router_a_address_g1"]}]
+        dictionary_nhs_full.update(dictionary_initial_read)
+        dictionary_nhs_full["dmvpn_addresses"][tfstate_dictionary["dmvpn_tunnel"]] = nhs_updates
 
-    with open('DB/dmvpn_tunnel_nhs_addresses.json', 'w') as new_json_data:
-        json.dump(dictionary_nhs_full, new_json_data, sort_keys=True, indent=4,
-                  ensure_ascii=False)
+    # Write the new dmvpn_tunnel_nhs_addresses file
+    with io.open('DB/dmvpn_tunnel_nhs_addresses.yml', 'w', encoding='utf8') as outfile:
+        yaml.dump(dictionary_nhs_full, outfile, default_flow_style=False, allow_unicode=True)
 
 
 if __name__ == "__main__":
